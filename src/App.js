2e9c1f293d8e1e9cd688814f29dbc986760a18ae
jaedon farr <jaedonfarr@gmail.com>
2024-04-21 02:31:45 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,19 +24,31 @@ function App() {
 
   const handleClick = (event) => {
     // send a request to prisoner api
-    superagent.post("https://appgateway.drc.ohio.gov/OffenderSearch/Search/SearchResults")
-    .send(searchPayload) // some of these "may" not need to be includeded in the payload
-    .set("accept", "*/*")
-    .set("accept-language", "en-US,en;q=0.9")
-    .set("cache-control", "max-age=0")
-    .set("content-type", "application/x-www-form-urlencoded")
-    .set("sec-ch-ua", "\"Google Chrome\";v=\"123\", \"Not:A-Brand\";v=\"8\", \"Chromium\";v=\"123\"")
-    .set("sec-ch-ua-mobile", "?0")
-    .set("sec-ch-ua-platform", "\"macOS\"")
-    
-    .then(response => {
-      console.log(response);
-      console.log(response.status);
+    fetch("https://appgateway.drc.ohio.gov/OffenderSearch/Search/SearchResults", {
+    redirect: "follow",
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "accept-language": "en-US,en;q=0.9",
+      "cache-control": "max-age=0",
+      "content-type": "application/x-www-form-urlencoded",
+      "sec-ch-ua": "\"Google Chrome\";v=\"123\", \"Not:A-Brand\";v=\"8\", \"Chromium\";v=\"123\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1"
+    },
+    "referrer": "https://appgateway.drc.ohio.gov/OffenderSearch/Search/SearchResults",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": `__RequestVerificationToken=${verificationToken}&IsAuthenticated=&LastName=&FirstName=derrick&CntyCommitment=&CntyResidential=&ZipCode=&Status=I&PbDate=&NumPrefix=A&OffNumber=&Sort=N`,
+    "method": "POST",
+    "mode": "no-cors",
+    "credentials": "include"
+    }).then(response => response.text()).then(response => {
+      console.log("text: " + response);
+      setData(response);
     });
   };
 
